f1d06acbbaf5503e6e2c34c5af14decfed2fa685
Moin Khan <nurmoin05@gmail.com>
2022-05-24 20:38:59 +0600

    featured product added

--- src/pages/Home/FeatureProduct/FeatureProduct.js
+++ src/pages/Home/FeatureProduct/FeatureProduct.js
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './FeatureProduct.css';
+import SingleProduct from './SingleProduct/SingleProduct';
 
 const FeatureProduct = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch('services.json')
+        .then(res => res.json())
+        .then(data => setProducts(data))
+    }, []);
+
     return (
         <div className="FeatureProduct">
             <div className="container mx-auto">
                 <div className="featureProduct__title">
                     <h2 className="text-center text-black text-2xl uppercase font-semibold mb-10">Featured products</h2>
                 </div>
-                <div className="featureProduct__content grid grid-cols-3 gap-30">
-                    <div className="card bg-base-100 shadow-xl">
-                        <div className="card-body">
-                            <img src="" alt="" />
-                        </div>
-                    </div>
+                <div className="featureProduct__content grid grid-cols-3 gap-10">
+                    {
+                        products.map((product, index) => <SingleProduct key={index} product={product} />)
+                    }
                 </div>
             </div>
         </div>
---
+++ src/pages/Home/FeatureProduct/SingleProduct/SingleProduct.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const SingleProduct = ({product}) => {
+    const {name, img, description, price, minimum, available} = product;
+    console.log(product)
+    return (
+        <div className="SingleProduct w-[350px] mx-auto">
+            <div className="card bg-base-100 shadow-xl">
+                <div className="card-body">
+                    <img className="rounded" src={img} alt="" />
+                    <div className="card-title mx-2">
+                        <h3 className="text-black">{name}</h3>
+                    </div>
+                    <div className="cart-description text-left">
+                        <p className="text-accent text-lg mb-2">{description}</p>
+                        <p className="text-accent">Per unit price: {price}</p>
+                        <p className="text-accent">Available in stock: {available}</p>
+                        <p className="text-accent">Minumum order amount: {minimum}</p>
+                    </div>
+                    <div className="card-action">
+                        <button className="btn btn-accent hover:btn-primary text-white hover:text-black w-full mt-2">Buy now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SingleProduct;
