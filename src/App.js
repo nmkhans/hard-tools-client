8086d043f54ab143cb3b43603961f2519b23f55e
Moin Khan <nurmoin05@gmail.com>
2022-05-25 16:20:41 +0600

    register page design done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import ApplicationRoutes from './global/ApplicationRoutes/ApplicationRoutes';
 import Header from './global/Header/Header';
 import Footer from './global/Footer/Footer';
+import { useLocation } from 'react-router-dom';
 
 const App = () => {
+  const location = useLocation();
+  const [showSection, setShowSection] = useState(false);
+  useEffect(() => {
+    if (location.pathname === "/register" || location.pathname === "/login") {
+      setShowSection(false);
+    } else {
+      setShowSection(true);
+    }
+  }, [location.pathname])
+
   return (
     <div className="App">
-      <Header />
+      {showSection && <Header />}
       <ApplicationRoutes />
-      <Footer />
+      {showSection && <Footer />}
     </div>
   );
 }
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,42 +1,114 @@
 import React from 'react';
-import register from './register.png';
+import img from './register.png';
 import { Link } from 'react-router-dom';
+import { useForm } from "react-hook-form";
 
 const Register = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = (data) => {
+        console.log(data)
+    };
+
     return (
-        <div className="Register">
+        <div className="Register py-10">
             <div className="container mx-auto">
                 <div className="register__content">
                     <div className="hero min-h-screen">
                         <div className="hero-content flex-col lg:flex-row justify-between">
                             <div className="register__img">
-                                <img className="w-[600px]" src={register} alt="" />
+                                <img className="w-[600px]" src={img} alt="" />
                             </div>
                             <div className="card flex-shrink-0 w-full max-w-lg shadow-2xl bg-base-100">
                                 <div className="card-title justify-center items-center pt-7">Register</div>
-                                <div className="card-body">
-                                    <div className="form-control">
-                                        <label className="label">
-                                            <span className="label-text">Email</span>
-                                        </label>
-                                        <input type="text" placeholder="email" className="input input-bordered" />
-                                    </div>
-                                    <div className="form-control">
-                                        <label className="label">
-                                            <span className="label-text">Password</span>
-                                        </label>
-                                        <input type="text" placeholder="password" className="input input-bordered" />
+                                <form onSubmit={handleSubmit(onSubmit)}>
+                                    <div className="card-body">
+                                        <div className="form-control">
+                                            <label className="label">
+                                                <span className="label-text">Name</span>
+                                            </label>
+                                            <input type="text" placeholder="Name" className="input input-bordered"
+                                                {...register("name", {
+                                                    required: {
+                                                        value: true,
+                                                        message: 'Name is required',
+                                                    }
+                                                })} />
+                                            <label>
+                                                {errors.name?.type === 'required' && <p className="font-semibold text-red-500">
+                                                    {errors.name.message}
+                                                </p>}
+                                            </label>
+                                        </div>
+                                        <div className="form-control">
+                                            <label className="label">
+                                                <span className="label-text">Email</span>
+                                            </label>
+                                            <input type="text" placeholder="email" className="input input-bordered"
+                                                {...register("email", {
+                                                    required: {
+                                                        value: true,
+                                                        message: 'Email is required',
+                                                    },
+                                                    pattern: {
+                                                        value: /[A-z0-9]+@[a-z]+\.[a-z]{1,2}/,
+                                                        message: 'Provide a valid Email'
+                                                    }
+                                                })} />
+                                            <label>
+                                                {errors.email?.type === 'required' && <p className="font-semibold text-red-500">
+                                                    {errors.email.message}
+                                                </p>}
+                                                {errors.email?.type === 'pattern' && <p className="font-semibold text-red-500">
+                                                    {errors.email.message}
+                                                </p>}
+                                            </label>
+                                        </div>
+                                        <div className="form-control">
+                                            <label className="label">
+                                                <span className="label-text">Password</span>
+                                            </label>
+                                            <input type="text" placeholder="password" className="input input-bordered"
+                                                {...register("password", {
+                                                    required: {
+                                                        value: true,
+                                                        message: 'Password is required',
+                                                    },
+                                                    minLength: {
+                                                        value: 6,
+                                                        message: "Password must be 6 character or longer"
+                                                    }
+                                                })} />
+                                            <label className="label">
+                                                {errors.password?.type === 'required' && <p className="font-semibold text-red-500">
+                                                    {errors.password.message}
+                                                </p>}
+                                                {errors.password?.type === 'minLength' && <p className="font-semibold text-red-500">
+                                                    {errors.password.message}
+                                                </p>}
+                                            </label>
+                                        </div>
+                                        <div className="form-control">
+                                            <label className="label">
+                                                <span className="label-text">Profile Image</span>
+                                            </label>
+                                            <input type="file" className="input input-bordered"
+                                                {...register("image")}
+                                            />
+                                        </div>
                                         <label className="text-center mt-5">
                                             Already have an account? <Link className="text-secondary" to="/login">Login</Link>
                                         </label>
+                                        <div className="form-control mt-6">
+                                            <button className="btn btn-primary text-black">Register</button>
+                                        </div>
                                     </div>
-                                    <div className="form-control mt-6">
-                                        <button className="btn btn-primary">Login</button>
-                                    </div>
-                                </div>
+                                </form>
                             </div>
                         </div>
                     </div>
+                </div>
+                <div className="goHome">
+                    <Link to="/" className="btn btn-secondary">Go Home</Link>
                 </div>
             </div>
         </div>
