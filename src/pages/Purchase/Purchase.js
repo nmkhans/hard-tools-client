2e3298db00518cc7bcbe7d3563018d6aa34ed139
Moin Khan <nurmoin05@gmail.com>
2022-05-26 21:19:34 +0600

    purchase form added

--- src/pages/Purchase/Purchase.js
+++ src/pages/Purchase/Purchase.js
@@ -1,23 +1,152 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useQuery } from 'react-query';
+import { useForm } from "react-hook-form";
+import Loading from './../../global/Loading/Loading';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from './../../firebase.init';
 
 const Purchase = () => {
+    const [user, loading] = useAuthState(auth);
+    const { id } = useParams();
+    const url = `http://localhost:5000/products/${id}`;
+    const { register, handleSubmit, formState: { errors }, reset } = useForm();
+
+    const { data: product, isLoading } = useQuery('purchase-item', () => (
+        fetch(url)
+            .then(res => res.json())
+    ))
+
+    if (isLoading || loading) {
+        return <Loading />
+    }
+
+    const onSubmit = async (data) => {
+        console.log(data);
+        reset();
+    };
+
+    const { displayName, email } = user;
+    const { name, img, price, description, minimum, available } = product;
+
     return (
         <div className="Purchase">
             <div className="hero min-h-screen bg-base-100">
                 <div className="hero-content text-center">
-                    <div className="max-w-md">
-                        <div className="card lg:card-side bg-base-100 shadow-2xl">
-                            <figure>
-                                <img src="" alt="Album" />
+                    <div className="max-w-2xl">
+                        <div className="bg-base-100 shadow-2xl flex sm:flex-col md:flex-row lg:flex-row justify-between items-center rounded-xl">
+                            <figure className="w-2/4">
+                                <img className="rounded-xl" src={img} alt="Album" />
                             </figure>
-                            <div className="card-body">
-                                
+                            <div className="purchase__detail text-left p-5">
+                                <h3 className="text-4xl text-black font-bold  mb-5">{name}</h3>
+                                <h4 className="text-accent mb-2">Price per unit: {price}</h4>
+                                <p className="text-accent mb-2">Available: {available}</p>
+                                <p className="text-accent mb-2">Minimum Amount: {minimum}</p>
+                                <p className="text-gary-600 text-lg mb-2">{description}</p>
                             </div>
                         </div>
                     </div>
+                    <div className="purchase__form">
+                        <div className="card w-[500px] shadow-2xl bg-base-100">
+                            <form onSubmit={handleSubmit(onSubmit)}>
+                                <div className="card-body">
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text">Name</span>
+                                        </label>
+                                        <input type="text" placeholder="Name" className="input input-bordered" defaultValue={displayName} readOnly
+                                            {...register("name")} />
+                                    </div>
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text">Email</span>
+                                        </label>
+                                        <input type="text" placeholder="email" className="input input-bordered"
+                                            defaultValue={email} readOnly
+                                            {...register("email")} />
+                                    </div>
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text">Amount</span>
+                                        </label>
+                                        <input type="number" placeholder="Amount" className="input input-bordered" defaultValue={minimum}
+                                            {...register("amount", {
+                                                required: {
+                                                    value: true,
+                                                    message: 'Minimum amount is required',
+                                                },
+                                                min: {
+                                                    value: minimum,
+                                                    message: `Amount has to be above ${minimum}`
+                                                },
+                                                max: {
+                                                    value: available,
+                                                    message: `Amount has to be below ${available}`
+                                                },
+
+                                            })} />
+                                        <label>
+                                            {errors.amount?.type === 'required' && <p className="font-semibold text-red-500">
+                                                {errors.amount.message}
+                                            </p>}
+                                            {errors.amount?.type === 'min' && <p className="font-semibold text-red-500">
+                                                {errors.amount.message}
+                                            </p>}
+                                            {errors.amount?.type === 'max' && <p className="font-semibold text-red-500">
+                                                {errors.amount.message}
+                                            </p>}
+                                        </label>
+                                    </div>
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text">Phone</span>
+                                        </label>
+                                        <input type="text" placeholder="Phone" className="input input-bordered"
+                                            {...register("phone", {
+                                                required: {
+                                                    value: true,
+                                                    message: "Phone is required"
+                                                }
+                                            })}
+                                        />
+                                        <label>
+                                            {
+                                                errors?.phone?.type === 'required' && <p className="font-semibold text-red-500">
+                                                    {errors?.phone?.message}
+                                                </p>
+                                            }
+                                        </label>
+                                    </div>
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text">Address</span>
+                                        </label>
+                                        <input type="text" placeholder="Address" className="input input-bordered"
+                                            {...register("address", {
+                                                required: {
+                                                    value: true,
+                                                    message: "Address is required"
+                                                }
+                                            })} />
+                                            <label>
+                                            {
+                                                errors?.address?.type === 'required' && <p className="font-semibold text-red-500">
+                                                    {errors?.address?.message}
+                                                </p>
+                                            }
+                                        </label>
+                                    </div>
+                                    <div className="form-control mt-6">
+                                        <button className="btn btn-primary">Place Order</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+            </div >
+        </div >
     );
 };
 
